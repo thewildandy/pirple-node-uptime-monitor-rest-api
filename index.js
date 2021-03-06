48750afe38dbfbc6d13206c64b92f15abf6363f2
thewildandy <thewildandy@gmail.com>
2021-03-06 23:22:33 +0100

    Refactor the handlers to their own file

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 const config = require('./config');
 const fs = require('fs');
+const handlers = require('./lib/handlers');
 const _data = require('./lib/data');
 
 // Instantiate a HTTP server
@@ -35,24 +36,6 @@ const httpsServer = https.createServer(httpsOptions, (request, response) => {
 httpsServer.listen(config.server.httpsPort, () => {
   console.log('The HTTPS server is up, listening on port ' + config.server.httpsPort + '.');
 });
-
-// Define our request handlers
-const handlers = {};
-
-// Define a low-impact handler to use to monitor uptime of this server
-// Very meta.
-handlers.ping = (data, callback) => {
-  callback(200, { message: 'The service is up.' });
-}
-
-handlers.test = (data, callback) => {
-  // Return all the data we interpretted from the request as the response
-  callback(200, data);
-};
-
-handlers.notFound = (data, callback) => {
-  callback(404);
-};
 
 // Define a simple router to resolve to handlers
 const router = {
